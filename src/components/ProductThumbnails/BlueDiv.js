8dd93dc29e097c4a5c69329acc3508341ccf63ea
Tory Klingenstein <toryklingenstein@gmail.com>
2019-11-01 13:49:05 -0400

    update button hover

--- src/components/ProductThumbnails/BlueDiv.js
+++ src/components/ProductThumbnails/BlueDiv.js
@@ -1,21 +1,39 @@
 import React from "react";
-import blueButtons from "./Images/blueButtons.svg";
+import whiteCart from "./Images/whiteCart.svg";
+import whiteHeart from "./Images/whiteHeart.svg";
 
-export default function BlueDiv() {
+export default function BlueDiv(props) {
   return (
-    <div style={blueDivStyle}>
-      <img src={blueButtons} style={blueStyle} />
+    <div style={blueStyle}>
+      <div style={blueButtonStyle}>
+        <button style={blueRound} onClick={props.onClick}>
+          <img src={whiteHeart} />
+        </button>
+        <button style={blueRound} onClick={props.onClick}>
+          <img src={whiteCart} />
+        </button>
+      </div>
     </div>
   );
 }
-
-const blueStyle = {
-  width: "102px",
+const blueRound = {
+  borderRadius: "50px",
+  border: "1px solid #33A0FF",
+  backgroundColor: "#33A0FF",
   height: "46px",
-  margin: "40px 0"
+  width: "46px",
+  padding: "14px"
+};
+const blueButtonStyle = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  height: "46px",
+  width: "102px",
+  margin: "40px auto"
 };
 
-const blueDivStyle = {
+const blueStyle = {
   textAlign: "center",
   width: "236px",
   height: "153px",
--- src/components/ProductThumbnails/WhiteDiv.js
+++ src/components/ProductThumbnails/WhiteDiv.js
@@ -1,15 +1,33 @@
 import React from "react";
-import whiteButtons from "./Images/whiteButtons.svg";
+import blueCart from "./Images/blueCart.svg";
+import blueHeart from "./Images/blueHeart.svg";
 
-export default function WhiteDiv() {
+export default function WhiteDiv(props) {
   return (
     <div style={whiteStyle}>
-      <img style={whiteButtonStyle} src={whiteButtons} />
+      <div style={whiteButtonStyle}>
+        <button style={whiteRound} onClick={props.onClick}>
+          <img src={blueHeart} />
+        </button>
+        <button style={whiteRound} onClick={props.onClick}>
+          <img src={blueCart} />
+        </button>
+      </div>
     </div>
   );
 }
-
+const whiteRound = {
+  borderRadius: "50px",
+  border: "1px solid #33A0FF",
+  backgroundColor: "white",
+  height: "46px",
+  width: "46px",
+  padding: "14px"
+};
 const whiteButtonStyle = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
   height: "46px",
   width: "102px",
   margin: "40px auto"
